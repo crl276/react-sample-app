b0d2b0338277b4b5ec5af4f50dff10078f6be75c
Callan Lamb <callan.lamb@gmail.com>
2017-05-06 21:19:14 -0400

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Header from './Header';
+import Home from './Home';
 import React from 'react';
 import { connect } from 'react-redux';
 
@@ -11,6 +13,7 @@ class App extends React.Component {
     return (
       <div>
       	<Header appName={this.props.appName} />
+        <Home />
       </div>
     );
   }
