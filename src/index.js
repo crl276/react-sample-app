3fd6ac9e4bbecd0f6182708111d266e5bc727049
Callan Lamb <callan.lamb@gmail.com>
2017-05-06 21:02:59 -0400

    Initialize git

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import React from 'react';
+import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 
-class App extends React.Component {
-  render() {
-    return (
-      <h1>Hello, World!</h1>
-    );
-  }
-}
+const defaultState = {
+	appName: 'conduit',
+	articles: null
+};
+
+const reducer = function(state = defaultState, action) {
+	return state;
+};
+
+
 
 ReactDOM.render((
-  <App />
-), document.getElementById('root'));
+  <Provider store={store}>
+  	<App />
+  </Provider>
+), document.getElementById('main'));
